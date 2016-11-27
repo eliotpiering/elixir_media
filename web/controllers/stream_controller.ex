6c696d3035359d87e4ccea9fb240898dc9eb516d
Eliot <eliot@launchcode.org>
2016-11-26 18:30:38 -0600

    stream a portion of the mp3 instead of the whole thing;

--- web/controllers/stream_controller.ex
+++ web/controllers/stream_controller.ex
@@ -3,12 +3,30 @@ defmodule ElixirMedia.StreamController do
 
   alias ElixirMedia.Stream
 
-  def show(conn, %{"id" => id}) do
+  def show(%{req_headers: headers} = conn, %{"id" => id}) do
     song = Repo.get!(ElixirMedia.Song, id)
     path = song.path
+    offset = get_offset(headers)
+    file_size = get_file_size(path)
+
     conn
     |> Plug.Conn.put_resp_header("content-type", "audio/mp3")
-    |> Plug.Conn.send_file(200, path)
+    |> Plug.Conn.put_resp_header("content-range", "bytes #{offset}-#{file_size-1}/#{file_size}")
+    |> Plug.Conn.send_file(206, path)
+  end
+
+  defp get_offset(headers) do
+    case List.keyfind(headers, "range", 0) do
+      {"range", "bytes=" <> start_pos} ->
+        String.split(start_pos, "-") |> hd |> String.to_integer
+      nil ->
+        0
+    end
+  end
+
+  defp get_file_size(path) do
+    {:ok, %{size: size}} = File.stat path
+    size
   end
 
 end
